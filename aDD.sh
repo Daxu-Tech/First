30503671816422fb0b61b84dfbe4049b1e26c480
Dishank <pateldishank64@gmail.com>
2022-02-18 16:39:12 +0530

    automated

--- aDD.sh
+++ aDD.sh
@@ -1,10 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
 git add .
 echo "Type commit :"
 read msg
 
+git config --global user.name "Dishank"
+git config --global user.email "pateldishank64@gmail.com"
 git commit -m $msg
-git push
-
+#git push
+#echo "Hii"
+#ping google.com
 
