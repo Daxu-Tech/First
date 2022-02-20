3cd41d33808b0581d5ddf221f3c566b64af63e08
Patel Dishank <pateldishank64@gmail.com>
2022-02-20 23:19:17 +0530

    Scan feature in branch

--- main.sh
+++ main.sh
@@ -8,8 +8,33 @@ function permanantAlias(){
 	source ~/.bashrc
 }
 
+function Scan(){
+        sudo nmap -sS 198.168.1.0/24 -oG scan.txt
+        cat scan.txt | grep Host | cut -d ' ' -f2 | sort -u
+}
+
+echo " What you want to perform ? "
+echo " 1.Permanent Alias"
+echo " 2.Scan a network"
+read choice
+
+
+case $choice in
+
+   1)
 	echo "Type keyword you want : "
         read -e short
         echo "Type actual keyword : "
         read -e actual
         permanantAlias $short $actual
+	;;
+
+   2)
+	LogMonitor
+	;;
+
+   *)
+	echo "Error"
+	;;
+
+esac
